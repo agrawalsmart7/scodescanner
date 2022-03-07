9b1807e078d452b03ea2103879514fa3b4717ae4
Utkarsh Agrawal <uagrawal@Utkarshs-MacBook-Pro.local>
2022-03-07 14:02:45 +0530

    deleted_test_files

--- uploads/final/vulnerablefolder/example1.php
+++
@@ -1,1 +0,0 @@
-<?php header(“Location: “. $_GET["go"]); die();  ?>
--- uploads/final/vulnerablefolder/ldap.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dn = $_GET['host'];
-$filter="(|(sn=$person*)(givenname=$person*))";
-$justthese = array("ou", "sn", "givenname", "mail");
-$sr=ldap_search($ds, $dn, $dn, $justthese);
-$info = ldap_get_entries($ds, $sr);
-echo $info["count"]." entries returned
-";?>
